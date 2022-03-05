4139ac667a4e27d063700d0ecdc7c2b227a787be
James Sullivan <jsullivan@cloudanswers.com>
2022-03-05 11:38:15 +0100

    handle other message types

--- telegram_bot/index.js
+++ telegram_bot/index.js
@@ -6,37 +6,61 @@ const storage = require("../storage");
 const bot = new Telegraf(settings.BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply(
-    "Keyboard",
-    Markup.inlineKeyboard([
-      Markup.button.callback("First option", "first"),
-      Markup.button.callback("Second option", "second"),
-    ])
-  );
-
   let message = ` Please use /info to get info`;
   ctx.reply(message);
 });
 
 bot.command("info", async (ctx) => {
   try {
-    ctx.reply("Not yet implemented...");
+    ctx.reply(
+      "Just forward message to the bot and it will be published on uainfo.pl (this is a work in progress)"
+    );
   } catch (error) {
     console.error("ERROR", error);
     ctx.reply("error :(");
   }
 });
 
-bot.on("sticker", (ctx) => ctx.reply("✔️"));
 bot.on("text", async (ctx) => {
   console.log("on text:", ctx);
   let fileName = await storage.log("messages", ctx);
-  ctx.reply(`got it! ${fileName}`);
+  ctx.reply(`got it! https://uainfo.pl/${fileName}`);
 });
 
-bot.on("*", (ctx) => {
-  console.log("on *:", ctx);
-  // TODO log to bucket
+bot.on("document", async (ctx) => {
+  console.log("on text:", ctx);
+  let fileName = await storage.log("documents", ctx);
+  ctx.reply(`got it! https://uainfo.pl/${fileName}`);
+});
+
+bot.on("edited_message", async (ctx) => {
+  console.log("on text:", ctx);
+  let fileName = await storage.log("edited_message", ctx);
+  ctx.reply(`got it! https://uainfo.pl/${fileName}`);
+});
+
+bot.on("forward_date", async (ctx) => {
+  console.log("on text:", ctx);
+  let fileName = await storage.log("forward_date", ctx);
+  ctx.reply(`got it! https://uainfo.pl/${fileName}`);
+});
+
+bot.on("location", async (ctx) => {
+  console.log("on text:", ctx);
+  let fileName = await storage.log("location", ctx);
+  ctx.reply(`got it! https://uainfo.pl/${fileName}`);
+});
+
+bot.on("photo", async (ctx) => {
+  console.log("on text:", ctx);
+  let fileName = await storage.log("photo", ctx);
+  ctx.reply(`got it! https://uainfo.pl/${fileName}`);
+});
+
+bot.on("video", async (ctx) => {
+  console.log("on text:", ctx);
+  let fileName = await storage.log("video", ctx);
+  ctx.reply(`got it! https://uainfo.pl/${fileName}`);
 });
 
 bot.launch();
